207e035ff7b2ed65a254461183dc3576f0121165
2tefan <me@2tefan.eu>
2025-07-15 22:31:04 +0200

    Draw some fun stuff

--- models/star/src/lib.rs
+++ models/star/src/lib.rs
@@ -14,49 +14,18 @@ use fj::{
 };
 
 pub fn model(
-    num_points: u64,
-    r1: f64,
-    r2: f64,
+    coords: Vec<[f64; 2]>,
     h: f64,
     core: &mut fj::core::Core,
 ) -> Solid {
-    let num_vertices = num_points * 2;
-    let vertex_iter = (0..num_vertices).map(|i| {
-        let angle_rad = 2. * PI / num_vertices as f64 * i as f64;
-        let radius = if i % 2 == 0 { r1 } else { r2 };
-        (angle_rad, radius)
-    });
-
-    let mut outer_points = Vec::new();
-    let mut inner_points = Vec::new();
-
-    for (angle_rad, radius) in vertex_iter {
-        let (sin, cos) = angle_rad.sin_cos();
-
-        let x = cos * radius;
-        let y = sin * radius;
-
-        outer_points.push([x, y]);
-        inner_points.push([x / 2., y / 2.]);
-    }
-
     let bottom_surface = core.layers.topology.surfaces.xy_plane();
-    let sweep_path = Vector::from([0., 0., -h]);
+    let sweep_path = Vector::from([0., 0., h]);
 
     Sketch::empty(&core.layers.topology)
         .add_regions(
             [Region::polygon(
-                outer_points,
+                coords,
                 core.layers.topology.surfaces.space_2d(),
-                core,
-            )
-            .add_interiors(
-                [Cycle::polygon(
-                    inner_points,
-                    core.layers.topology.surfaces.space_2d(),
-                    core,
-                )
-                .reverse(core)],
                 core,
             )],
             core,
--- models/star/src/main.rs
+++ models/star/src/main.rs
@@ -1,39 +1,82 @@
+use std::{fs::File, io::BufReader};
+
 use clap::Parser;
+use fj::{core::{operations::{build::{BuildRegion, BuildSketch, BuildSolid}, merge::Merge, sweep::SweepSketch, transform::TransformObject, update::UpdateSketch}, topology::{Region, Sketch, Solid}}, math::{Scalar, Vector}};
 
 #[derive(Parser)]
-struct Parameters {
-    /// Number of points in the star
-    #[arg(long, default_value = "5")]
-    points: u64,
-
-    /// Inner radius of the star
-    #[arg(long, default_value = "1.0")]
-    inner_radius: f64,
-
-    /// Outer radius of the star
-    #[arg(long, default_value = "2.0")]
-    outer_radius: f64,
+struct Building {
+    #[clap(skip)]
+    coords: Vec<[f64; 2]>,
 
     /// Height of the star
-    #[arg(long, default_value = "1.0")]
+    #[arg(long, default_value = "5.0")]
     height: f64,
 
     #[command(flatten)]
     fj: fj::Args,
 }
 
+pub fn model_cubio(size: impl Into<Vector<3>>, core: &mut fj::core::Core) -> Solid {
+    let [x, y, z] = size.into().components;
+
+    let bottom_surface = core.layers.topology.surfaces.xy_plane();
+    let sweep_path = Vector::from([Scalar::ZERO, Scalar::ZERO, z]);
+
+    Sketch::empty(&core.layers.topology)
+        .add_regions(
+            [Region::polygon(
+                [
+                    [-x / 2., -y / 2.],
+                    [x / 2., -y / 2.],
+                    [x / 2., y / 2.],
+                    [-x / 2., y / 2.],
+                ],
+                core.layers.topology.surfaces.space_2d(),
+                core,
+            )],
+            core,
+        )
+        .sweep_sketch(bottom_surface, sweep_path, core)
+}
+
+
+fn load_coords(path: &str) -> std::io::Result<Vec<Vec<[f64; 2]>>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let coords = serde_json::from_reader(reader)?;
+    Ok(coords)
+}
+
 fn main() -> fj::Result {
     let mut fj = fj::Instance::new();
-    let params = Parameters::parse();
+    let params = Building::parse();
 
-    let model = star::model(
-        params.points,
-        params.inner_radius,
-        params.outer_radius,
-        params.height,
-        &mut fj.core,
-    );
-    fj.process_model_args(&model, params.fj)?;
+    let osm_coords_a = vec![
+        [[0.0, 0.0], [1.0, -0.2], [1.0, 2.], [0.5, 4.2]],
+        [[3.0, 0.0], [1.2, 0.0], [1.2, 0.5], [0.0, 0.2]],
+        [[0.1, 2.0], [1.2, 0.0], [4.2, 0.5], [2.0, 0.2]],
+    ];
+    let osm_coords = load_coords("/tmp/buildings.json").expect("Failed to load JSON");
+
+    let mut all = Solid::empty();
+
+    for coords in osm_coords {
+        let vec_coords =   coords.to_vec();
+        println!("{:#?}", vec_coords);
+        let model = star::model(
+            vec_coords,
+            params.height,
+            &mut fj.core,
+        );
+
+        all = all.merge(&model, &mut fj.core);
+    }
+
+
+    let base_plate = model_cubio([210.0, 210.0, -1.], &mut fj.core);
+    all = all.merge(&base_plate, &mut fj.core);
+
+    fj.process_model_args(&all, params.fj)?;
 
     Ok(())
 }
